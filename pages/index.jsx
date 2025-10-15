e15a7633dff1e2c199b8e97b36f05aea3d648c7a
esraakanat <kntesraa82@gmail.com>
2025-10-16 02:09:09 +0300

    fix image path for esraa.JPEG

--- pages/index.jsx
+++ pages/index.jsx
@@ -91,7 +91,7 @@ const Index1 = () => {
         <section
           id="home"
           className="home-banner-01 bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: "url(static/img/esraa.jpeg)" }}
+          style={{ backgroundImage: "url(/static/img/esraa.JPEG)" }}
         >
           <div className="container">
             <div className="row full-screen align-items-center p-100px-tb">
